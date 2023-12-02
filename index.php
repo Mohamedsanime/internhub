5c0c463974c7705b8364091f4e31c5197944a189
Mohamedsanime <yasannouma@gmail.com>
2023-12-02 17:44:02 +0400

    Load files

--- index.php
+++ index.php
@@ -1,101 +1,164 @@
 <?php
 include('admin/includes/header.php');
-include('admin/includes/topbar.php');
-include('admin/includes/sidebar.php');
 ?>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Dashboard</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+<!DOCTYPE html>
+<html lang="en">
 
-        <!-- Main content -->
-        <section class="content">
-      <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-              <div class="inner">
-                <h3>150</h3>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-                <p>New Orders</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-bag"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-success">
-              <div class="inner">
-                <h3>53<sup style="font-size: 20px">%</sup></h3>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <script src="https://kit.fontawesome.com/75e0b79c22.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="admin/assets/dist/css/main.css">
 
-                <p>Bounce Rate</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-stats-bars"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-              <div class="inner">
-                <h3>44</h3>
+    <title>Internship Management System</title>
+    <style>
+        body {
+        background-image: url('admin/assets/dist/img/bg.png');
+    }
+    </style>
 
-                <p>User Registrations</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-person-add"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-danger">
-              <div class="inner">
-                <h3>65</h3>
+</head>
 
-                <p>Unique Visitors</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-pie-graph"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
+<body>
+ 
+    <div class="container mt-3">
+        <div class="header d-flex flex-row justify-content-between align-items-center ">
+            <a href="index.html" class="header_logo d-inline-flex text-decoration-none align-items-center text-white">
+                <img src="admin/assets/dist/img/emulogo3.png" alt="" class="brand-image img-circle elevation-3" style="opacity: .7" />
+                <div class="ms-2">
+                    <h1 class="text-uppercase fw-bold fs-6">
+                       Eastern Mediterraneen University
+                    </h1>
+                    <span class="text-capitalize">Internship Management System</span>
+                </div>
+            </a>
+            <nav class="menu">
+                <ul class="d-flex list-unstyled">
+                <li class="nav-item">
+                        <a href="admin/register.php" class="nav-link text-white">Sign Up</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="admin/forgetpwd.php" class="nav-link text-white">Forgot Password?</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="admin/changepwd.php" class="nav-link text-white">Change Password</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="admin/contactus2.php" class="nav-link text-white">Contact Us </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
-        <!-- /.row -->
 
-             <!-- /.row (main row) -->
-      </div><!-- /.container-fluid -->
-    </section>
-</div>
-<?php
-include('admin/includes/footer.php');
-?>
+    </div>
+
+    <div class="container">
+        <div class="login-section d-flex align-items-center justify-content-center my-1">
+
+                <div class="card px-2 py-3" style="width: 22rem;">
+                    <div class="card-body">
+                        <?php
+                        require ("config.php");
+                       
+                        if (isset($_POST["email"]) || isset($_POST["password"]) ){
+                            #echo '<script>alert("Welcome to Geeks test123")</script>'; 
+                            $email= $_POST["email"];
+                            $password= md5($_POST["password"]);
+                            # alert("{$email}");
+                            $query = $db->prepare("SELECT users.id,name,surname,email,role_name 
+                                    FROM users INNER JOIN roles ON users.rol_id = roles.id 
+                                    WHERE email=:vemail AND passwordHash=:vpasswordHash");
+                            $query->execute([
+                                    "vemail" =>$email,
+                                    "vpasswordHash" =>$password
+                            ]);
+                            $data = $query->fetch(PDO::FETCH_ASSOC);
+
+                            $control = $query->rowCount();
+                            //print_r($data);
+
+                            if($control==0){
+                                // Redirect If Login Failed;
+                                #echo '<script>alert("Welcome to Geeks for Geeks")</script>'; 
+                                echo("Login Failed ");
+                                header("Location:index.php");
+                            }else{
+                               # echo '<script>alert("Welcome to Geeks 123456")</script>'; 
+                              
+                                echo("Login In ");
+                                session_start();
+                                
+                                $_SESSION["users"] = $data;
+                                $_SESSION["login"] = true;
+                                header("Location:management/index.php");
+
+                                echo "<script type='text/javascript'>alert('{$data["role_name"]}');</script>"; 
+                            }
+                           
+                            //echo "<script type='text/javascript'>alert('{$password}');</script>"; 
+
+
+
+
+                        }
+                        if (isset($_POST["register"]) )
+                        {
+                            header("admin/register.php");
+                        }
+
+
+                        //print_r($data);
+                        ?>
+                        <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+                            <div class="mb-3 d-flex justify-content-center">
+                                <img src="admin/assets/dist/img/interhublogo.jpg" alt="" class="" width="125" height="125">
+                            </div>
+                            <div class="mb-3">
+                                <label for="mail" class="form-label">Email address:</label>
+                                <input type="text" class="form-control" id="mail" name="email" required
+                                       placeholder="123456@emu.edu.tr">
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password:</label>
+                                <input type="password" class="form-control" id="password" name="password" required
+                                       placeholder="Enter Password">
+                            </div>
+                            <button type="submit"  class="btn btn-primary" name="login">Login</button>
+                            <button type="submit"  class="btn btn-primary" name="register" style="margin-left:145px;">Register</button>
+                        </form>
+
+                    </div>
+                </div>
+
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="footer">
+            <span class="text-white small"><center>Mohamed Sanime Amira, Ahmad Zyadoun Alkhatib, Allwell Dennis Ufot</center></span>
+        </div>
+    </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
